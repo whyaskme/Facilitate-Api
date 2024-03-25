708f59e34cc6cb9ca6650f29ed76db74dbc81608
Joseph Baranauskas <joseph.baranauskas@gmail.com>
2024-03-25 11:25:59 -0500

    Correct Libraries project reference.

--- obj/Debug/net8.0/Api.AssemblyInfo.cs
+++ obj/Debug/net8.0/Api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+16543a447aac90a8ba398b4049bf51ea4d31eb17")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+656518c9d5f656709def68b2d7a60b65dd099dfe")]
 [assembly: System.Reflection.AssemblyProductAttribute("Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
